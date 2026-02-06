823d46736ca1e9faf2f595741fab2a802aee6e68
Martin Behrendts <info@novusidea.de>
2026-02-06 19:35:56 +0100

    If image is not set hide it

--- app/components/AppAvatar.vue
+++ app/components/AppAvatar.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 const props = defineProps<{
-    image: string,
-    name: string,
+    image?: string,
+    name?: string,
 }>();
 </script>
 
 <template>
-    <img :src="image" :alt="'Profilfoto von ' + name" class="inline rounded-full border-[0.1rem] border-primary object-cover" />
+    <img v-if="image" :src="image" :alt="'Profilfoto von ' + name" class="inline rounded-full border-[0.1rem] border-primary object-cover" />
 </template>
