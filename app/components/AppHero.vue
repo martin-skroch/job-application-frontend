fff6ce9ce7a4e0825b35aaa13d9df3132d49fcd2
Martin Behrendts <info@novusidea.de>
2026-02-11 10:08:42 +0100

    Removed dump

--- app/components/AppHero.vue
+++ app/components/AppHero.vue
@@ -21,8 +21,6 @@ const props = defineProps<{scrollTarget?: string}>();
             <AppMetaNavigation />
         </div>
 
-        <pre>{{ props.scrollTarget }}</pre>
-
         <a v-if="props.scrollTarget" :href="'#' + props.scrollTarget" role="button" class="absolute bottom-0 left-1/2 -translate-x-1/2 p-4 no-hover">
             <svg class="stroke-current opacity-30 w-12 p-2 -m-2 h-auto stroke-[0.04em] animate-bounce" viewBox="0 0 29.712 8.8547" xmlns="http://www.w3.org/2000/svg">
                 <path d="m29.476 0.44055-14.62 7.8467-14.62-7.8467" fill="none" stroke="currentColor" />
