d8d2eadfc0a9b83d2b8250f8c55ed5be3a2430a8
Martin Behrendts <info@novusidea.de>
2026-02-15 14:14:51 +0100

    Created an component for images

---
+++ app/components/AppImage.vue
@@ -0,0 +1,3 @@
+<template>
+    <img loading="lazy" class="size-3/5 md:size-full group-even:ms-auto group-odd:-rotate-2 group-even:rotate-2 aspect-square object-cover bg-current rounded-2xl border border-current shadow-[0_0.5rem_0_0_currentColor]" />
+</template>
