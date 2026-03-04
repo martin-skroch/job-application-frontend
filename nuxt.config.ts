b5bcde9fc4df8da72a21106781242a7a51aa41e5
Martin Behrendts <info@novusidea.de>
2026-03-04 11:40:52 +0100

    Added meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 const siteUrl = process.env.NUXT_SITE_URL || ''
 
+const title = 'Martin Skroch - Full Stack Web Developer';
+const description = 'Erfahrener Entwickler mit Fokus auf PHP und Laravel sowie fundierten Kenntnissen in JavaScript, Vue.js und Tailwind CSS – spezialisiert auf moderne, performante Webanwendungen vom Backend bis zum Frontend.';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
@@ -21,13 +24,14 @@ export default defineNuxtConfig({
                 lang: 'de',
                 prefix: 'og: http://ogp.me/ns#',
              },
-            title: 'Martin Skroch - Full Stack Web Developer',
+            title: title,
             meta: [
                 { property: 'og:type', content: 'website' },
                 { property: 'og:locale', content: 'de_DE' },
-                { property: 'og:title', content: 'Martin Skroch - Full Stack Web Developer' },
+                { property: 'og:title', content: title },
                 { property: 'og:image', content: `${siteUrl}/images/og-image.webp` },
-                { property: 'og:description', content: 'Erfahrener Entwickler mit Fokus auf PHP und Laravel sowie fundierten Kenntnissen in JavaScript, Vue.js und Tailwind CSS – spezialisiert auf moderne, performante Webanwendungen vom Backend bis zum Frontend.' },
+                { property: 'og:description', content: description },
+                { name: 'description', content: description },
             ],
             link: [
                 { rel: 'shortcut icon', type: 'image/x-icon', href: `${siteUrl}/favicon.ico` },
